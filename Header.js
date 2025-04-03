a1db7d6853b45f0bb7dc83b63d4af5952176a50c
Priyanka9pri <priyanka.4968@chitkara.edu.in>
2025-04-03 21:23:46 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,11 @@
+<header>
+    <h1>MMTTP Faculty Development Programme</h1>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About AI</a></li>
+            <li><a href="modules.html">Program Structure</a></li>
+            <li><a href="feedback.html">Feedback</a></li>
+        </ul>
+    </nav>
+</header>
